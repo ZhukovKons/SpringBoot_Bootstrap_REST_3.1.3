1c15373ef052050a58c4416aef8b4057751f80c8
ZhukovKons <konstantinzhukov91@bk.ru>
2021-06-08 10:55:40 +0300

    first commit

---
+++ src/main/java/ru/pre/project/JM/JmApplication.java
@@ -0,0 +1,13 @@
+package ru.pre.project.JM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pre/project/JM/JmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pre.project.JM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
